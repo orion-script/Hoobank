18c5e2f07cb052a0c61686f2bdf3a03d36543473
Olaniyi John Ayomide <jaolaniyi96@student.lautech.edu.ng>
2022-12-25 14:04:36 +0100

    fix

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function SideBar() {
   return (
-    <div className="bg-gradient-to-r from-background via-gray-800 to-gray-700 text-white nav-bar absolute w-2/5 rounded-lg ml-44 hidden">
+    <div className="bg-gradient-to-r from-background via-gray-800 to-gray-700 text-white nav-bar absolute w-2/6 rounded-lg hidden left-48">
       <ul className="flex flex-col items-center py-5">
         <li className="cursor-pointer py-1">Home</li>
         <li className="cursor-pointer py-1">Features</li>
